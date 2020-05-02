6335a8567771405d16a9d6b2810bd8f93aea3cc5
LoyalFlames <nicksonlangat95@gmail.com>
2020-05-02 10:11:00 +0300

    changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_ez^*iv)7fj%(2l^z+--3c@8*w4o^wj-q0p9o)uhvcsex(0+=j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
